dbeb29cd3531943fb9207b32470644f7a0f4bcfd
Henry Paulino <henrypl360@gmail.com>
2020-11-15 23:02:47 -0500

    write simple test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Creates user' do
+    user = User.new(uid: 1, name: 'Test User', email: 'test@stocket.com')
+
+    assert_equal user.name, 'Test User'
+    assert_equal user.email, 'test@stocket.com'
+  end
 end
