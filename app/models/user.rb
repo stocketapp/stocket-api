020eb2be4a814bc2ab1a5997c7d950d023028d9a
Henry Paulino <henrypl360@gmail.com>
2020-11-16 18:50:24 -0500

    unique email and uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_one :user_info
 
+  validates :email, presence: true, uniqueness: true
+  validates :uid, presence: true, uniqueness: true
+
   def user(user)
     User.find_by uid: user.uid
   end
