3fa70a1f588884ceaef7ab7e28bb2a4d853f10e7
Henry Paulino <henrypl360@gmail.com>
2020-11-18 16:17:34 -0500

    change redis host and port

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,6 +1,6 @@
 FirebaseIdToken.configure do |config|
   is_dev = Rails.env.development?
-  host = is_dev ? 'localhost' : 'stocket-api.redis.cache.windows.net'
+  host = is_dev ? 'localhost' : 'redis-11326.c240.us-east-1-3.ec2.cloud.redislabs.com'
   config.project_ids = ['stocket-dev', '']
-  config.redis = Redis.new(host: host, port: 6380, db: 0)
+  config.redis = Redis.new(host: host, port: 11_326, db: 0)
 end
