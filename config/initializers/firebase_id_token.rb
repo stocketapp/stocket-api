990e3b25536d61f1cd849cd492aeb78d08488a02
Henry Paulino <henrypl360@gmail.com>
2020-11-18 16:28:03 -0500

    use env variables for redis cache

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,6 +1,8 @@
 FirebaseIdToken.configure do |config|
   is_dev = Rails.env.development?
-  host = is_dev ? 'localhost' : 'redis-11326.c240.us-east-1-3.ec2.cloud.redislabs.com'
+  host = is_dev ? 'localhost' : ENV['REDIS_CACHE_HOST']
+  port = is_dev ? 6379 : ENV['REDIS_CACHE_PORT']
+  password = is_dev ? '' : ENV['REDIS_CACHE_PASSWORD']
   config.project_ids = ['stocket-dev', '']
-  config.redis = Redis.new(host: host, port: 11_326, db: 0)
+  config.redis = Redis.new(host: host, port: port, db: 0, password: password)
 end
