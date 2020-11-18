762a8abfeb1478a8b6da56fb02cbf5423b7e0039
Henry Paulino <henrypl360@gmail.com>
2020-11-18 16:34:34 -0500

    fix redis

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -2,7 +2,15 @@ FirebaseIdToken.configure do |config|
   is_dev = Rails.env.development?
   host = is_dev ? 'localhost' : ENV['REDIS_CACHE_HOST']
   port = is_dev ? 6379 : ENV['REDIS_CACHE_PORT']
-  password = is_dev ? '' : ENV['REDIS_CACHE_PASSWORD']
+  password = ENV['REDIS_CACHE_PASSWORD']
+  puts host
+  puts port
+  puts password
   config.project_ids = ['stocket-dev', '']
-  config.redis = Redis.new(host: host, port: port, db: 0, password: password)
+  config.redis = if is_dev
+                   Redis.new(host: host, port: port, db: 0, password: password)
+                 else
+                   Redis.new(host: host, port: port, db: 0)
+
+                 end
 end
