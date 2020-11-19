504240c916f6d8143d1267d78261fd42935f2a30
Henry Paulino <henrypl360@gmail.com>
2020-11-18 19:39:40 -0500

    cors setting and change query method names

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,15 +2,14 @@ require 'json'
 
 module Types
   class QueryType < Types::BaseObject
-    field :get_user_info, Types::UserInfoType, null: false
-    field :get_user, Types::UserType, null: false
+    field :get_user_info, Types::UserInfoType, null: false, method: :fetch_user
+    field :get_user, Types::UserType, null: false, method: :fetch_user_info
 
-    def get_user
-      user = User.find_by! uid: context[:current_user]['uid']
-      user
+    def fetch_user
+      User.find_by! uid: context[:current_user]['uid']
     end
 
-    def get_user_info      
+    def fetch_user_info
       UserInfo.find_by! id: context[:current_user]['id']
     end
   end
