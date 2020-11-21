f8ca570cbdc629cb8873f8863b963407e4cf5df9
Henry Paulino <henrypl360@gmail.com>
2020-11-21 17:07:37 -0500

    update firebase id token and docker compose

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,12 +1,6 @@
 FirebaseIdToken.configure do |config|
   is_dev = Rails.env.development?
-  host = is_dev ? 'localhost' : ENV['REDIS_CACHE_HOST']
-  port = is_dev ? 6379 : ENV['REDIS_CACHE_PORT']
-  password = ENV['REDIS_CACHE_PASSWORD']
+  password = is_dev ? 'password' : ENV['REDIS_CACHE_PASSWORD']
   config.project_ids = ['stocket-dev', '']
-  config.redis = if is_dev
-                   Redis.new(host: host, port: port, db: 0, password: password)
-                 else
-                   Redis.new(host: host, port: port, db: 0)
-                 end
+  config.redis = Redis.new(host: ENV['REDIS_CACHE_HOST'], port: 15680, db: 0, password: password)
 end
