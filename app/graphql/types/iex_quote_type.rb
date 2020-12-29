6b8007aaae6f7eafd00f113d421b6b71d1eeb0d6
Henry Paulino <henrypl360@gmail.com>
2020-12-29 16:27:03 -0500

    use BigInt

--- app/graphql/types/iex_quote_type.rb
+++ app/graphql/types/iex_quote_type.rb
@@ -6,9 +6,9 @@ module Types
     # field :sector # sector of the stock
     # field :calculation_price, null: true # source of the latest price - tops, sip, previousclose or close
     field :open, Float, null: true # official open price
-    field :openTime, Int, null: true # official listing exchange time for the open
+    field :openTime, GraphQL::Types::BigInt, null: true # official listing exchange time for the open
     field :close, Float, null: true # official close price
-    field :closeTime, Int, null: true # official listing exchange time for the close
+    field :closeTime, GraphQL::Types::BigInt, null: true # official listing exchange time for the close
     field :high, Float, null: true # market-wide highest price from the SIP, 15 minute delayed
     field :low, Float, null: true # market-wide lowest price from the SIP, 15 minute delayed
     field :latestPrice, Float, null: true # latest price being the IEX real time price, the 15 minute delayed market price, or the previous close price
@@ -26,7 +26,7 @@ module Types
     # field :extended_price, null: true # the pre market and post market price
     field :change, Float, null: true # change in value, calculated using calculation_price from previous_close
     field :changePercent, Float, null: true # change in percent, calculated using calculation_price from previous_close
-    field :marketCap, Int, null: true # market cap, calculated in real time using calculation_price
+    field :marketCap, GraphQL::Types::BigInt, null: true # market cap, calculated in real time using calculation_price
     field :peRatio, Float, null: true # PE ratio, calculated in real time using calculation_price
     field :week52High, Float, null: true # adjusted 52 week high
     field :week52Low, Float, null: true # adjusted 52 week low
