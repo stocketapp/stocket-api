a6397f05cdd8b169b4dc4cdcf0623e24bfbc3503
Henry Paulino <henrypl360@gmail.com>
2021-02-09 17:48:17 -0500

    remove unnecessary fetch of added watchlist stock

--- app/graphql/mutations/add_to_watchlist.rb
+++ app/graphql/mutations/add_to_watchlist.rb
@@ -2,11 +2,12 @@ module Mutations
   class AddToWatchlist < BaseMutation
     argument :symbol, String, required: true
 
-    type Types::IexQuoteType
+    field :id, ID, null: true
+    field :symbol, String, null: true
 
     def resolve(symbol: nil)
       user_id = context[:current_user][:id]
-      Watchlist.create!(user_id: user_id, symbol: symbol).get_quote
+      Watchlist.create!(user_id: user_id, symbol: symbol)
     end
   end
 end
