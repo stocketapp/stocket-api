20bd6ce420e92904a4ca62bb7de00ab55175f9b8
Henry Paulino <henrypl360@gmail.com>
2021-02-17 19:28:08 -0500

    add fields to trade query and arguments to trade mutation

--- app/graphql/mutations/add_to_watchlist.rb
+++ app/graphql/mutations/add_to_watchlist.rb
@@ -2,8 +2,8 @@ module Mutations
   class AddToWatchlist < BaseMutation
     argument :symbol, String, required: true
 
-    field :id, ID, null: true
-    field :symbol, String, null: true
+    field :id, ID, null: false
+    field :symbol, String, null: false
 
     def resolve(symbol: nil)
       user_id = context[:current_user][:id]
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,5 +1,21 @@
 class Trade < ApplicationRecord
+  belongs_to :user
+  validates :symbol, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :order_type, presence: true
+
   def self.get_trades(user)
     self.all
   end
 end
+
+
+# t.bigint "user_id"
+#     t.string "symbol"
+#     t.integer "quantity"
+#     t.decimal "price"
+#     t.decimal "total"
+#     t.string "order_type"
+#     t.date "order_date"
+#     t.index ["user_id"], name: "index_trades_on
