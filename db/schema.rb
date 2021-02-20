5ebc0b29a92ecdfc29fe26605d26db2eaa8630a6
Henry Paulino <henrypl360@gmail.com>
2021-02-19 20:37:26 -0500

    add trade reference id to positions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_005333) do
+ActiveRecord::Schema.define(version: 2021_02_20_013230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_02_20_005333) do
     t.decimal "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "trade_reference_id", null: false
     t.index ["user_id"], name: "index_positions_on_user_id"
   end
 
