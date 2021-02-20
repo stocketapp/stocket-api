032ea801b810d42178447e307910562281ed7aab
Henry Paulino <henrypl360@gmail.com>
2021-02-20 01:52:20 -0500

    migration to change quantity from decimal to int in positions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_013230) do
+ActiveRecord::Schema.define(version: 2021_02_20_064922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_02_20_013230) do
     t.bigint "user_id"
     t.string "symbol"
     t.decimal "price"
-    t.decimal "quantity"
+    t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "trade_reference_id", null: false
