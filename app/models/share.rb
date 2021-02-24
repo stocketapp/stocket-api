50a6a2a3703fa4c9661b9097a52fc9b431ed20b8
Henry Paulino <henrypl360@gmail.com>
2021-02-23 19:47:34 -0500

    add "buy" method that creates multiple shares on BUY

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,3 +1,22 @@
 class Share < ApplicationRecord
   belongs_to :user
+
+  def self.buy(t)
+    shares = []
+    for i in 1..t[:quantity] do
+      shares.push({
+        user_id: t[:user_id],
+        symbol: t[:symbol],
+        price: t[:price],
+        trade_reference_id: t[:reference_id]
+      })
+    end
+    Share.create!(shares) do |s|
+      user = User.find_by id: s[:user_id]
+      new_value = user[:cash] - t[:total]
+      
+      user.update!(cash: user[:cash])
+      puts "NEW_CASH ~> #{user[:cash]}"
+    end
+  end
 end
