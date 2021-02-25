d4a6208d5ca281377c707e4110d7c428d551b108
Henry Paulino <henrypl360@gmail.com>
2021-02-24 23:05:14 -0500

    test if selling a stock increases user cash

--- test/graphql/mutations/trade_mutations_test.rb
+++ test/graphql/mutations/trade_mutations_test.rb
@@ -15,7 +15,6 @@ module Mutations
       }
     GRAPHQL
     user_one = { current_user: { id: 1 }}
-    user_two = { current_user: { id: 2 }}
     buy_input = {
       symbol: 'AAPL',
       price: 123.95,
@@ -30,7 +29,7 @@ module Mutations
     }
     sell_input = {
       symbol: 'AAPL',
-      price: 123.95,
+      price: 124.95,
       quantity: 2,
       orderType: 'SELL'
     }
@@ -49,13 +48,13 @@ module Mutations
     end
 
     test 'Error when input is wrong' do
-      trade = StocketApiSchema.execute(mutation_string, variables: { input: wrong_input }, context: user_two)
+      trade = StocketApiSchema.execute(mutation_string, variables: { input: wrong_input }, context: user_one)
 
       assert !trade['errors'].nil?
     end
 
     test 'Add shares when order type is "BUY"' do
-      trade = StocketApiSchema.execute(mutation_string, variables: { input: buy_input }, context: user_two)
+      trade = StocketApiSchema.execute(mutation_string, variables: { input: buy_input }, context: user_one)
       reference_id = trade['data']['createTrade']['referenceId']
       shares = Share.where trade_reference_id: reference_id
 
@@ -63,18 +62,28 @@ module Mutations
     end
 
     test 'Decrease cash when buying' do
-      StocketApiSchema.execute(mutation_string, variables: { input: buy_input }, context: user_two)
-      user = User.find_by id: 2
+      StocketApiSchema.execute(mutation_string, variables: { input: buy_input }, context: user_one)
+      user = User.find_by id: 1
 
       assert_equal user[:cash], 18016.8
     end
 
     test 'Sell shares when order type is "SELL"' do
-      StocketApiSchema.execute(mutation_string, variables: { input: buy_input }, context: user_two)
-      trade = StocketApiSchema.execute(mutation_string, variables: { input: sell_input }, context: user_two)
+      StocketApiSchema.execute(mutation_string, variables: { input: buy_input }, context: user_one)
+      trade = StocketApiSchema.execute(mutation_string, variables: { input: sell_input }, context: user_one)
       shares_length = Share.where(symbol: trade['data']['createTrade']['symbol']).length
 
       assert_equal shares_length, 2
+    end
+
+    test 'Increase cash when selling' do
+      StocketApiSchema.execute(mutation_string, variables: { input: buy_input }, context: user_one)
+      old_cash = User.find_by(id: 1)[:cash]
+      trade = StocketApiSchema.execute(mutation_string, variables: { input: sell_input }, context: user_one)
+      total = trade['data']['createTrade']['total']
+      cash = User.find_by(id: 1)[:cash]
+
+      assert_equal cash, old_cash + total
     end
   end
 end
