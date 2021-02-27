24896ae91f6c81e2dcd4fc520d44b05ea87362c9
Henry Paulino <henrypl360@gmail.com>
2021-02-26 20:47:15 -0500

    add purchase value column to Shares

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_184556) do
+ActiveRecord::Schema.define(version: 2021_02_27_011116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_02_26_184556) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "size"
+    t.float "purchase_value"
     t.index ["user_id"], name: "index_shares_on_user_id"
   end
 
