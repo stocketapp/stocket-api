ab84db895afc9b5c30098b34188f3187c4228b21
Henry Paulino <henrypl360@gmail.com>
2021-02-26 19:19:24 -0500

    add more tests to position

--- test/graphql/queries/position_query_test.rb
+++ test/graphql/queries/position_query_test.rb
@@ -3,18 +3,53 @@ require 'test_helper'
 module Mutations
   class PositionQueryTest < ActionDispatch::IntegrationTest
     query_string = <<-GRAPHQL
-      query GetPosition($symbol: String) {
-        position(symbol: $symbol) {
+      query {
+        position(symbol: "AMZN") {
           symbol
-          pctChange
+          totalValue
+          change
+          changePct
+          avgPrice
+          positionSize
         }
       }
     GRAPHQL
     context = { current_user: { id: 1 }}
+    position_query = StocketApiSchema.execute(query_string, variables: nil, context: context)
 
-    test 'Something in Position' do
-      query = StocketApiSchema.execute(query_string, variables: { symbol: 'AMZN' }, context: context)
-      puts query['data']
+    test 'Can query a position' do
+      symbol = position_query['data']['position']['symbol']
+      assert_equal symbol, 'AMZN'
+    end
+
+    test 'Calculate the total value of the position' do
+      value = position_query['data']['position']['totalValue']
+
+      assert value
+    end
+
+    test 'Calculate the change of the position' do
+      value = position_query['data']['position']['change']
+
+      assert value
+    end
+
+    test 'Calculate the change percentage of the position' do
+      value = position_query['data']['position']['changePct']
+
+      assert value
+    end
+
+    test 'Calculate the average price of the position' do
+      value = position_query['data']['position']['avgPrice']
+
+      assert value
+    end
+
+    test 'Get the amount of shares owned' do
+      value = position_query['data']['position']['positionSize']
+
+      assert value
     end
   end
 end
