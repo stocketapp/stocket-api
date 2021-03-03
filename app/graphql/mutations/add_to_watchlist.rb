3c34cda82218255599d5d61cd36f93178f2f34d8
Henry Paulino <henrypl360@gmail.com>
2021-03-02 21:47:35 -0500

    fix add_to_watchlist mutation resolve

--- app/graphql/mutations/add_to_watchlist.rb
+++ app/graphql/mutations/add_to_watchlist.rb
@@ -8,7 +8,21 @@ module Mutations
     def resolve(symbol: nil)
       user_id = context[:current_user][:id]
       w = Watchlist.create!(user_id: user_id, symbol: symbol)
-      { id: w.id }.merge(w.iex_quote)
+      create_watchlist_item({ id: w.id }.merge(w.iex_quote))
+    end
+
+    private
+
+    def create_watchlist_item(item)
+      {
+        symbol: item.symbol,
+        change: item.change,
+        change_percent: item.changePercent,
+        company_name: item.companyName,
+        latest_price: item.latestPrice,
+        id: item.id,
+        logo: item.logo
+      }
     end
   end
 end
