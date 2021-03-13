ed7c25b150168264efd1796509a75d7dda55a18b
Henry Paulino <henrypl360@gmail.com>
2021-03-13 16:37:37 -0500

    fix test

--- test/graphql/queries/user_query_test.rb
+++ test/graphql/queries/user_query_test.rb
@@ -5,7 +5,7 @@ module Queries
     query_string = <<-GRAPHQL
       query {
         user {
-          portfolio
+          portfolioValue
         }
       }
     GRAPHQL
@@ -15,7 +15,7 @@ module Queries
       context = { current_user: { uid: '123' } }
       query = StocketApiSchema.execute(query_string, variables: nil, context: context)
 
-      assert_not query['data']['user']['portfolio'].nil?
+      assert_not query['data']['user']['portfolioValue'].nil?
     end
   end
 end
