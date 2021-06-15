4037a186fae01a759558546a724a6d4c02ac5972
Henry Paulino <henrypl360@gmail.com>
2021-06-15 18:47:06 -0400

    big performance improvements for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,44 +5,55 @@ class Portfolio
   def initialize(user_id:)
     @user = User.find_by id: user_id
     @shares = Share.where user_id: user_id
-    @balance_history = BalanceHistory.where(user_id: user_id).order('id desc').limit(1)[0]
     @value = calculate_portfolio_value(@shares)
-    @shares_symbols = @shares.map(&:symbol)
-    @shares_val = @shares.map { |el| { el.symbol => el.purchase_value } }
   end
 
   def change
-    print_f value: diff(value, @balance_history.nil? ? 0 : @balance_history[:value])
+    balance_history = BalanceHistory.where(user_id: @user.id).order('id desc').limit(1)[0]
+    print_f value: diff(@value, balance_history.nil? ? 0 : balance_history[:value])
   end
 
   def change_pct
-    print_f value: (change / value) * 100
+    print_f value: (change / @value) * 100
   end
 
   def positions
-    return calc_positions unless @shares_symbols.empty?
+    return calc_positions unless @shares.map(&:symbol).empty?
   end
 
   # TODO: Add a value with total money invested
 
   private
 
-  def calc_change_and_percentage(symbol, quotes)
-    shares = Share.where(user_id: @user.id, symbol: symbol)
-    prev_value = shares.map(&:purchase_value).sum
-    current_value = calc_value(shares, quotes[symbol]['quote']['latestPrice'])
-    p_change = calc_change(current_value, prev_value)
-    { change: current_value, change_pct: calc_change_pct(p_change, current_value) }
+  def calc_change_with_price(purchase_price, size, latest_price)
+    purchase_value = purchase_price * size
+    current_value = latest_price * size
+    calc_change(current_value, purchase_value)
+  end
+
+  def calc_change_and_pct(share, latest_price)
+    sum_change = calc_change_with_price(share.price, share.size, latest_price)
+    pct = calc_change_pct(sum_change, latest_price * share.size)
+    { change: sum_change, pct: pct }
+  end
+
+  def define_position(sym, quotes)
+    latest_price = quotes[sym]['quote']['latestPrice']
+    name = quotes[sym]['quote']['companyName']
+    pos = Share.where(user_id: 3, symbol: sym).map { |sh| calc_change_and_pct(sh, latest_price) }
+    change = pos.sum { |i| i[:change] }
+    pct = pos.sum { |i| i[:pct] }
+    { symbol: sym, change: change, change_pct: pct, logo: logo(sym), company_name: name }
   end
 
   def calc_positions
-    quotes = Share.fetch_iex_batch_quote(@shares_symbols.join(','))
-    @shares_symbols.map do |sym|
-      pos = calc_change_and_percentage(sym, quotes)
-      logo = "https://storage.googleapis.com/iex/api/logos/#{sym}.png"
-      company_name = quotes[sym]['quote']['companyName']
-      { symbol: sym, change: pos[:change], change_pct: pos[:change_pct], logo: logo, company_name: company_name }
-    end
+    shares_symbols = @shares.map(&:symbol)
+    quotes = Share.fetch_iex_batch_quote(shares_symbols.uniq.join(','))
+    shares_symbols.uniq.map { |sym| define_position(sym, quotes) }
+  end
+
+  def logo(symbol)
+    "https://storage.googleapis.com/iex/api/logos/#{symbol}.png"
   end
 
   # Calculates the total value based on the price passed
