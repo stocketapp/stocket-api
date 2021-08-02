e67307f385c5885d043d202a61f088e6ffc705ce
Henry Paulino <henrypl360@gmail.com>
2021-08-02 15:05:01 -0400

    fix typo

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -5,7 +5,7 @@ module Types
     field :exchange, String, null: false
     field :industry, String, null: false
     field :description, String, null: false
-    field :webstie, String, null: false
+    field :website, String, null: false
     field :ceo, String, null: false
     field :street, String, null: false
     field :state, String, null: false
