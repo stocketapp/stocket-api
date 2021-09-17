fb81d498f614f1905d7c80d640a332b005f58911
Henry Paulino <henrypl360@gmail.com>
2021-09-17 17:12:40 -0400

    use different redis db

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -2,6 +2,6 @@ FirebaseIdToken.configure do |config|
   password = ENV['REDIS_CACHE_PASSWORD']
   host = ENV['REDIS_CACHE_HOST']
   port = ENV['REDIS_CACHE_PORT']
-  config.project_ids = ['stocket-dev', '']
+  config.project_ids = %w[stocket-dev stocket-f0191]
   config.redis = Redis.new(host: host, port: port, db: 0, password: password)
 end
