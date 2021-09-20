a6b6dbf0a3e780cdbc9e442feb6378c56c9f0362
Henry Paulino <henrypl360@gmail.com>
2021-09-20 18:12:14 -0400

    fix balance

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,13 +2,8 @@
 class Portfolio
   # @return [Float]
   def self.calculate_portfolio_value(user_id)
-    new_arr = []
     shares = Share.where(user_id: user_id)
-    portfolio = create_portfolio(shares)
-    symbols = shares.map(&:symbol).uniq
-    quotes = ApplicationRecord.fetch_iex_batch_quote(symbols.join(','))
-    portfolio.each_pair { |k, v| new_arr.push(quotes[k]['quote']['latestPrice'] * v) }
-    (format '%.2f', new_arr.sum).to_f
+    shares.length.positive? ? value(shares) : 0.0
   end
 
   # @param [ActiveRecord::Relation<Share>] positions
@@ -17,5 +12,14 @@ class Portfolio
     obj = {}
     shares.each { |p| obj.key?(p.symbol) ? obj[p.symbol] += p.size : obj[p.symbol] = p.size }
     obj
+  end
+
+  def self.value(shares)
+    new_arr = []
+    portfolio = create_portfolio(shares)
+    symbols = shares.map(&:symbol).uniq
+    quotes = ApplicationRecord.fetch_iex_batch_quote(symbols.join(','))
+    portfolio.each_pair { |k, v| new_arr.push(quotes[k]['quote']['latestPrice'] * v) }
+    (format '%.2f', new_arr.sum).to_f
   end
 end
