f13bd698339b18dea8284baaf463f01d13741659
Henry Paulino <henrypl360@gmail.com>
2021-09-28 16:16:23 -0400

    fix position query tests

--- test/graphql/queries/position_query_test.rb
+++ test/graphql/queries/position_query_test.rb
@@ -7,14 +7,14 @@ module Queries
         position(symbol: "AMZN") {
           symbol
           totalValue
-          change
-          changePct
+          change24h
+          change24hPct
           avgPrice
-          positionSize
+          size
         }
       }
     GRAPHQL
-    context = { current_user: { id: 1 }}
+    context = { current_user: { id: 1 } }
     position_query = StocketApiSchema.execute(query_string, variables: nil, context: context)
 
     test 'Can query a position' do
@@ -29,25 +29,26 @@ module Queries
     end
 
     test 'Calculate the change of the position' do
-      value = position_query['data']['position']['change']
+      value = position_query['data']['position']['change24h']
 
       assert value
     end
 
     test 'Calculate the change percentage of the position' do
-      value = position_query['data']['position']['changePct']
+      value = position_query['data']['position']['change24hPct']
 
       assert value
     end
 
     test 'Calculate the average price of the position' do
+      puts position_query.inspect
       value = position_query['data']['position']['avgPrice']
 
       assert value
     end
 
     test 'Get the amount of shares owned' do
-      value = position_query['data']['position']['positionSize']
+      value = position_query['data']['position']['size']
 
       assert value
     end
